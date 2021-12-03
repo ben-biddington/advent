861cdb322bea0278a0fd64c7941b9cc7f2353f16
Ben Biddington <ben.biddington@gmail.com>
2021-12-03 18:45:21 +1300

    [GREEN] Day 3 part one

--- tests/day-03/day-3-examples.ts
+++ tests/day-03/day-3-examples.ts
@@ -1,21 +1,103 @@
 import { expect } from 'chai';
 import * as fs from 'fs';
-import { sum } from '../../core/array-extensions';
 
-describe('--- Day 3: --- (part one)', () => {
-  it('the given example', () => {
+const mostCommonBit = (reports: string[], index: number) => {
+  let zeroes = 0;
+  let ones = 0;
+
+  reports.forEach(report => {
+    const value = report[index];
+
+    if (value === '0') {
+      zeroes++;
+    } else {
+      ones++;
+    }
+  });
+
+  return ones > zeroes ? 1 : 0; // what about equality?
+}
+
+const leastCommonBit = (reports: string[], index: number) => {
+  let zeroes = 0;
+  let ones = 0;
+
+  reports.forEach(report => {
+    const value = report[index];
+
+    if (value === '0') {
+      zeroes++;
+    } else {
+      ones++;
+    }
+  });
+
+  return ones < zeroes ? 1 : 0; // what about equality?
+}
+
+const gammaRate = (input: string) => {
+  const reports = input.split('\n').map(it => it.trim()).filter(it => it.length > 0);
+
+  const result = [];
+
+  const width = reports[0].length;
+
+  for (let i = 0; i < width; i++) {
+    result[i] = mostCommonBit(reports, i);
+  }
+
+  return parseInt(result.join(''), 2);
+}
+
+const epsilonRate = (input: string) => {
+  const reports = input.split('\n').map(it => it.trim()).filter(it => it.length > 0);
+
+  const result = [];
+
+  const width = reports[0].length;
+
+  for (let i = 0; i < width; i++) {
+    result[i] = leastCommonBit(reports, i);
+  }
+
+  return parseInt(result.join(''), 2);
+}
+
+describe('--- Day 3: --- Binary Diagnostic (part one)', () => {
+  it(`You need to use the binary numbers in the diagnostic report to generate two new binary numbers 
+  
+    (called the gamma rate and the epsilon rate). 
+  
+    The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
+
+    Each bit in the gamma rate can be determined by finding the most common bit in the corresponding 
+    position of all numbers in the diagnostic report. For example, given the following diagnostic report:`, () => {
+
     const raw = `
-    forward 5
-    down 5
-    forward 8
-    up 3
-    down 8
-    forward 2
-    `;
+    00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010
+    `
+    expect(gammaRate(raw)).to.eql(22);
+    expect(epsilonRate(raw)).to.eql(9);
+    expect(epsilonRate(raw) * gammaRate(raw)).to.eql(198);
+  });
 
-    const expected = { horizontal: 15, depth: 10 }
+  it(`Real report`, () => {
+    const raw = fs.readFileSync('./input/three/input').toString(); 
+    
+    // [!] Ouch, the data on file is wider than the example: more digits than the 5 shown above.
 
-    expect(expected).to.eql(actual);
+    expect(epsilonRate(raw) * gammaRate(raw)).to.eql(3309596);
   });
 });
 
