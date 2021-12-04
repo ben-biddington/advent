bd14ce87fe7735f3a1be4a28481b4f45a1b18728
Ben Biddington <ben.biddington@gmail.com>
2021-12-04 13:20:11 +1300

    Refactor: duplication (1)

--- tests/day-01/day-1-examples.ts
+++ tests/day-01/day-1-examples.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import * as fs from 'fs';
 import { sum } from 'core/array-extensions';
+import { lines } from 'core/internal/text';
 
 const getIncreases = (numbers: number[]) => {
   return numbers.filter((value, index) => {
@@ -32,9 +33,7 @@ const getIncreasesInThreeMeasurementSlidingWindow = (numbers: number[]) => {
 describe('--- Day 1: Sonar Sweep --- (part one)', () => {
   it('the number of times a depth measurement increases', () => {
     // https://adventofcode.com/2021/day/1#part1
-    const input: number[] = fs.readFileSync('./input/one/input').toString()
-      .split('\n')
-      .map(it => it.trim())
+    const input: number[] = lines(fs.readFileSync('./input/one/input').toString())
       .map(it => parseInt(it));
   
     expect(getIncreases(input).length).to.eql(1557);
@@ -57,22 +56,15 @@ describe('--- Day 1: Sonar Sweep --- (part two)', () => {
       263
     `;
 
-    const input: number[] = raw
-      .split('\n')
-      .map(it => it.trim())
-      .map(it => parseInt(it))
-      .filter(it => false == isNaN(it));
-
-    expect(input.length).to.eql(10);
+    const input: number[] = lines(raw)
+      .map(it => parseInt(it));
 
     expect(getIncreasesInThreeMeasurementSlidingWindow(input).length).to.eql(5);
   });
 
   it('three-measurement sliding window', () => {
     // https://adventofcode.com/2021/day/1#part2
-    const input: number[] = fs.readFileSync('./input/one/input').toString()
-      .split('\n')
-      .map(it => it.trim())
+    const input: number[] = lines(fs.readFileSync('./input/one/input').toString())
       .map(it => parseInt(it));
   
     expect(getIncreasesInThreeMeasurementSlidingWindow(input).length).to.eql(1608);
