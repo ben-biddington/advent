1de2c7829d47cc19cfcb90299ac64101f326f294
Ben Biddington <ben.biddington@gmail.com>
2021-12-04 13:05:47 +1300

    Refactor: stepdown rule

--- core/submarine/life-support.ts
+++ core/submarine/life-support.ts
@@ -2,31 +2,6 @@ import { fromBinary } from "core/internal/numbers";
 import { lines } from "core/internal/text";
 import { BitCriteria, leastCommonBit, mostCommonBit } from "../internal/bit-vector";
 
-// Select the matching reports by BitCriteria
-const filterBy = (reports: string[], index: number, criteria: BitCriteria) => {
-  if (criteria == BitCriteria.MostComon) {
-    const theMostCommonBit = mostCommonBit(reports, index).toString();
-
-    if (theMostCommonBit === 'tie') {
-      // In the fifth position, there are an equal number of 0 bits and 1 bits (one each). 
-      // So, to find the oxygen generator rating, keep the number with a 1 in that position: 10111.
-      return reports.filter((report: string) => report[index] == '1');  
-    }
-
-    return reports.filter((report: string) => report[index] == theMostCommonBit);
-  }
-
-  const theLeastCommonBit = leastCommonBit(reports, index).toString();
-
-  if (theLeastCommonBit === 'tie') {
-    // In the third position, there are an equal number of 0 bits and 1 bits (one each). 
-    // So, to find the CO2 scrubber rating, keep the number with a 0 in that position: 01010.
-    return reports.filter((report: string) => report[index] == '0');  
-  }
-
-  return reports.filter((report: string) => report[index] == theLeastCommonBit);
-}
-
 export const oxygenGeneratorRating = (input: string) => {
   const reports = lines(input);
 
@@ -63,4 +38,29 @@ export const carbonDioxideGeneratorRating = (input: string) => {
     throw new Error(`There are <${filteredReports.length}> reports, expected 1.`);
 
   return fromBinary(filteredReports[0]);
+}
+
+// Select the matching reports by BitCriteria
+const filterBy = (reports: string[], index: number, criteria: BitCriteria) => {
+  if (criteria == BitCriteria.MostComon) {
+    const theMostCommonBit = mostCommonBit(reports, index).toString();
+
+    if (theMostCommonBit === 'tie') {
+      // In the fifth position, there are an equal number of 0 bits and 1 bits (one each). 
+      // So, to find the oxygen generator rating, keep the number with a 1 in that position: 10111.
+      return reports.filter((report: string) => report[index] == '1');  
+    }
+
+    return reports.filter((report: string) => report[index] == theMostCommonBit);
+  }
+
+  const theLeastCommonBit = leastCommonBit(reports, index).toString();
+
+  if (theLeastCommonBit === 'tie') {
+    // In the third position, there are an equal number of 0 bits and 1 bits (one each). 
+    // So, to find the CO2 scrubber rating, keep the number with a 0 in that position: 01010.
+    return reports.filter((report: string) => report[index] == '0');  
+  }
+
+  return reports.filter((report: string) => report[index] == theLeastCommonBit);
 }
