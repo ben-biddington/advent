178fbf4e36ed77b2aa52dcc3b4549aa0614666b7
Ben Biddington <ben.biddington@gmail.com>
2021-12-08 09:20:22 +1300

    Day 7 complete

--- core/array-extensions.ts
+++ core/array-extensions.ts
@@ -1,1 +1,14 @@
 export const sum = (array: number[]) => array.reduce((a,b) => a + b, 0);
+export const range = (from: number, length: number): number[] => {
+  const to = from + length;
+
+  const result = new Array(to - from);
+
+  console.log({from, to});
+
+  for (let index = from; index <= to; index++) {
+    result[index] = index;    
+  }
+
+  return result;
+}
--- tests/day-07-examples.ts
+++ tests/day-07-examples.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { sum } from "core/array-extensions";
+import { range, sum } from "core/array-extensions";
 import * as fs from 'fs';
 
 const minimize = (input: string) => {
@@ -26,7 +26,60 @@ const minimize = (input: string) => {
   return min;
 }
 
-describe.only('--- Day 7: The Treachery of Whales --- (part one)', () => {
+const fuelUsed = (moveCount: number) => {
+  let result = 0;
+
+  for (let index = 1; index <= moveCount; index++) {
+    result += index;    
+  }
+
+  return result;
+
+  return sum(range(1, moveCount));
+}
+
+// Two calculations: distance and fuel used
+const minimizeFuel = (input: string) => {
+  const distances = input
+    .split(',')
+    .map(it => parseInt(it))
+    .sort((a, b) => a - b);
+
+  const fuelCache = new Map<number,number>();
+
+  const getFuelUsed = (distance: number) : number => {
+    if (false == fuelCache.has(distance)) { 
+      fuelCache.set(distance, fuelUsed(distance));
+    }
+    
+    return fuelCache.get(distance) || 0;
+  }
+
+  let last    = 0;
+
+  // Start from zero and move right until numbers start to increase
+  for (let position = 0; position >= 0; position++) {
+    const current = sum(distances.map(n => {
+        const distanceMoved = Math.max(position, n) - Math.min(position, n);
+     
+        return getFuelUsed(distanceMoved);
+      }
+    ));
+
+    if (position == 0) {
+      last = current;
+    } else if (position > 0) {
+      if (current > last)
+        break;
+      
+      last = current;
+    }
+  }
+
+  return last;
+}
+
+describe('--- Day 7: The Treachery of Whales --- (part one)', () => {
   it(`This is the cheapest possible outcome (the given example)`, () => {
     const input = `16,1,2,0,4,2,7,1,2,14`;
     expect(minimize(input)).to.eql(37);
@@ -36,5 +89,18 @@ describe.only('--- Day 7: The Treachery of Whales --- (part one)', () => {
     const input = fs.readFileSync('./input/seven').toString();
 
     expect(minimize(input)).to.eql(353800);
+  });
+});
+
+describe('--- Day 7: The Treachery of Whales --- (part two)', () => {
+  it(`Minimise fuel`, () => {
+    const input = `16,1,2,0,4,2,7,1,2,14`;
+    expect(minimizeFuel(input)).to.eql(168);
+  });
+
+  it(`Real example`, () => {
+    const input = fs.readFileSync('./input/seven').toString();
+
+    expect(minimizeFuel(input)).to.eql(98119739);
   });
 });
