f44f382bd40e84a3ea590bfb9581309530539519
Ben Biddington <ben.biddington@gmail.com>
2021-12-07 20:57:41 +1300

    Day 7 part one

---
+++ tests/day-07-examples.ts
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { sum } from "core/array-extensions";
+import * as fs from 'fs';
+
+const minimize = (input: string) => {
+  const distances = input
+    .split(',')
+    .map(it => parseInt(it))
+    .sort((a, b) => a - b);
+
+  let max = distances[distances.length-1] * 5;
+  let min = max;
+
+  // https://www.geeksforgeeks.org/optimum-location-point-minimize-total-distance/
+
+  for (let distance = max; distance >= 0; distance--) {
+    const current = sum(distances.map(n => {
+      return Math.max(distance, n) - Math.min(distance, n);
+    }));
+
+    if (current < min) {
+      min = current;
+    }
+  }
+
+  return min;
+}
+
+describe.only('--- Day 7: The Treachery of Whales --- (part one)', () => {
+  it(`This is the cheapest possible outcome (the given example)`, () => {
+    const input = `16,1,2,0,4,2,7,1,2,14`;
+    expect(minimize(input)).to.eql(37);
+  });
+});
