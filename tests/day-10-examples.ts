1f072bb6856afa8d91a6708f001c9c3edd88d26b
Ben Biddington <ben.biddington@gmail.com>
2021-12-11 20:53:06 +1300

    Day 10 part two complete

--- tests/day-10-examples.ts
+++ tests/day-10-examples.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import { sum } from "core/array-extensions";
 import { lines } from "core/internal/text";
+import { reverse } from "dns";
 import * as fs from 'fs';
 
 type Token = {
@@ -89,7 +90,7 @@ const score = (characters: string []) => {
   ]);
 }
 
-describe.only('--- Day 10: Syntax Scoring ---', () => {
+describe('--- Day 10: Syntax Scoring --- (part one)', () => {
   it('abc', () => {
     expect(() => parse('{([(<{}[<>[]}>{[]{[(<()>')).to.throw('Expected ], but found } instead.');
     expect(() => parse('[[<[([]))<([[{}[[()]]]')  ).to.throw('Expected ], but found ) instead');
@@ -126,5 +127,101 @@ describe.only('--- Day 10: Syntax Scoring ---', () => {
 
     //@ts-ignore
     expect(score(errors)).to.eql(392043);
+  });
+});
+
+const middleCompletionScore = (input: string): number => {
+  const completions = scoreCompletions(input).sort((a,b) => a - b);
+  const middleIndex = Math.round(completions.length/2) - 1;
+  
+  return completions[middleIndex];
+}
+
+const scoreCompletions = (input: string): number[] => {
+  const lines       = incompleteLines(input);
+  const completions = lines.map(complete);
+
+  return completions.map(scoreCompletion);
+}
+
+// The score is determined by considering the completion string character-by-character. 
+// Start with a total score of 0. 
+// Then, for each character, 
+// 
+//  multiply the total score by 5 and then increase the total score by the point value given for the character
+//  in the following table:
+//
+//     ): 1 point.
+//     ]: 2 points.
+//     }: 3 points.
+//     >: 4 points.
+//
+const scoreCompletion = (completion: string) => {
+  const scores = new Map<string,number>([
+    [ ')', 1],
+    [ ']', 2],
+    [ '}', 3],
+    [ '>', 4]
+  ]);
+
+  const tokens = completion.split('');
+
+  return tokens.reduce((previous, current, index) => {
+    return (previous * 5) + (scores.get(tokens[index]) || 0)
+  }, 0);
+}
+
+// Assume NO illegal characters
+const complete = (input: string | undefined) => {
+  const tokens = new Tokens();
+
+  const openingTokens: string[] = [];
+
+  (input || '').split('')
+    .map(it => it.trim())
+    .filter(it => it.length > 0)
+    .forEach((token) => {
+      if (tokens.isOpening(token)) {
+        openingTokens.push(token);
+      } else {
+        const lastOpeningToken = openingTokens[openingTokens.length - 1]; 
+        const expectedOpening  = tokens.openingFor(token);
+
+        if (expectedOpening === lastOpeningToken) {
+          openingTokens.pop();
+        }
+      }
+  });
+
+  return openingTokens.map(tokens.closingFor).reverse().join('');
+}
+
+const incompleteLines = (input: string) => {
+  return lines(input).map(line => {
+    return firstIllegal(line) === undefined ? line : undefined;
+  }).filter(it => it !== undefined);
+}
+
+describe('--- Day 10: Syntax Scoring --- (part two)', () => {
+  it('Find completion sequence', () => {
+    const input = `
+      [({(<(())[]>[[{[]{<()<>>
+      [(()[<>])]({[<{<<[]>>(
+      {([(<{}[<>[]}>{[]{[(<()>
+      (((({<>}<{<{<>}{[]{[]{}
+      [[<[([]))<([[{}[[()]]]
+      [{[{({}]{}}([{[{{{}}([]
+      {<[[]]>}<{[{[{[]{()[[[]
+      [<(<(<(<{}))><([]([]()
+      <{([([[(<>()){}]>(<<{{
+      <{([{{}}[<[[[<>{}]]]>[]]`;
+
+    expect(middleCompletionScore(input)).to.eql(288957);
+  });
+
+  it('The real example', () => {
+    const input = fs.readFileSync('./input/ten').toString();
+
+    expect(middleCompletionScore(input)).to.eql(1605968119);
   });
 });
