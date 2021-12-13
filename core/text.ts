349efc11d83c8f58c76596931395b7ea35fb616a
Ben Biddington <ben.biddington@gmail.com>
2021-12-13 21:20:24 +1300

    Day 13, basic parsing

--- core/text.ts
+++ core/text.ts
@@ -1,4 +1,10 @@
-export const lines = (text: string) => text.split('\n').map(it => it.trim()).filter(it => it.length > 0);
+export type Options = { allowBlank?: boolean }
+
+export const lines = (text: string, opts: Options = { allowBlank: false }) => 
+  text.split('\n').map(it => it.trim()).filter(it => {
+    return (opts.allowBlank) ? true : it.length > 0;
+  });
+
 export const isLowerCase = (value: string) => {
   const match = value.toLowerCase().match(new RegExp(`${value}`));
 
--- tests/day-13-examples.ts
+++ tests/day-13-examples.ts
@@ -2,20 +2,47 @@ import { expect } from "chai";
 import Path from "core/pathing/path";
 import { Segment, Segments } from "core/pathing/segments";
 import { lines } from "core/text";
+import { isThisISOWeek } from "date-fns";
 import * as fs from 'fs';
 
-const parse = (input: string) => {
-  const paths: Segment[] = lines(input)
-    .map(line => {
-      const [start, end] = line.split('-');
-      return { start, end };
-    });
-  
-  return new Segments(paths);
+class Origami {
+  private input: string[] = [];
+
+  constructor(input: string[]) {
+    this.input = input;
+  }
+
+  get count() {
+    return this.input.length;
+  }
+}
+
+type Instructions = {
+  lines: string[];
+}
+
+const indexOf = (lines: string[], match: (line: string) => boolean) => {
+  let index = -1;
+
+  lines.forEach((line, i) => {
+    if (match(line)) {
+      index = i;
+    }
+  });
+
+  return index;
+}
+
+const parse = (input: string): [Origami, Instructions] => {
+  const l = lines(input.trim(), { allowBlank: true });
+
+  const indexOfBlankLine = indexOf(l, line => line.trim().length === 0);
+
+  return [new Origami(l.slice(0, indexOfBlankLine)), { lines: l.slice(indexOfBlankLine + 1) }];
 }
 
 describe('--- Day 13: Transparent Origami --- (part one)', () => {
-  it('The given example', () => {
+  it('Parsing', () => {
     const input = `
       6,10
       0,14
@@ -35,10 +62,15 @@ describe('--- Day 13: Transparent Origami --- (part one)', () => {
       2,14
       8,10
       9,0
-      
+        
       fold along y=7
       fold along x=5
     `;
+
+    const [origami, instructions] = parse(input);
+
+    expect(origami.count).to.eql(18);
+    expect(instructions.lines.length).to.eql(2);
 
     const expected = `
       ...#..#..#.
