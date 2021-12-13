d319a8d3f8d5181c978f0afd8b65ef0fe234d64d
Ben Biddington <ben.biddington@gmail.com>
2021-12-13 14:36:53 +1300

    Refactor: use history

--- core/pathing/history.ts
+++ core/pathing/history.ts
@@ -1,13 +1,25 @@
+import { isLowerCase } from "core/text";
+
 export class DefaultHistory implements History {
-  private history: Map<string,number> = new Map();
+  private history: Map<string,number>;
+
+  constructor(history: Map<string,number> | null = null) {
+    this.history = history || new Map();
+  }
 
   record(cave: string) {
     this.history.set(cave, (this.history.get(cave) || 0) + 1);
   }
 
-  allow(cave: string) {
-    console.log(this.caves);
-    return false == this.caves.includes(cave);
+  // Disallow any lowercase cave that is not start or end
+  allow = (cave: string) => {
+    return cave == 'end' || false == this.caves
+      .filter(isLowerCase)
+      .includes(cave);
+  }
+
+  clone() {
+    return new DefaultHistory(new Map(this.history));
   }
 
   private get caves() {
@@ -18,4 +30,5 @@ export class DefaultHistory implements History {
 export interface History {
   record: (cave: string) => void;
   allow: (cave: string) => boolean;
+  clone: () => History;
 }
--- core/pathing/path.ts
+++ core/pathing/path.ts
@@ -1,19 +1,15 @@
-import { isLowerCase } from "../text";
 import { DefaultHistory, History } from "./history";
 import { Segments } from "./segments";
 
 export default class Path {
   private readonly segments: Segments;
   private progress: string[] = [];
-  private smallCaves: string[] = [];
   private history: History;
-  private log: (m: any) => void = (m) => this._debug ? console.log(m) : () => {};
   private _debug: boolean = false;
 
   constructor(segments: Segments, history: History | null = null, progress: string[] = []) {
     this.segments = segments;
     this.progress = progress;
-    this.smallCaves = progress.filter(isLowerCase);
     this.history = history || new DefaultHistory();
   }
 
@@ -23,13 +19,10 @@ export default class Path {
 
   follow(start: string) : string[] {
     this.progress.push(start);
+    this.history.record(start);
 
     if (start === 'end') {
       return [this.progressReport()];
-    }
-
-    if (isLowerCase(start)) {
-      this.smallCaves.push(start);
     }
 
     // [!] Can go backwards or forwards
@@ -38,23 +31,18 @@ export default class Path {
       ...this.segments.endingAt(start).map(it => it.start).filter(it => it != 'start')
     ];
 
-    const nextAvailableCaves = allDestinations
-      .filter(it  => false === this.smallCaves.includes(it));
+    const nextAvailableCaves = allDestinations.filter(this.history.allow);
     
-    this.log({ 
-      progress: this.progressReport(),
-      current: start,
-      'reverse-destinations': this.segments.endingAt(start).map(it => it.start).filter(it => it != 'start'),
-      nextAvailableCaves
-    });
-  
     if (nextAvailableCaves.length === 0) {
       return []; // Never made it to 'end'
     }
 
     return nextAvailableCaves
       .map((cave) => {
-        const path = new Path(this.segments, this.history, [...this.progress]); // [!] Copying progress very important!
+        const path = new Path(
+          this.segments, 
+          this.history.clone(), // [!] Copying progress very important!
+          [...this.progress]);  // [!] Copying progress very important!
         
         if (this._debug) {
           path.debug();
