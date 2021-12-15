fc2ed173e1bd4a42a24e1dba732b74b09fb6e279
Ben Biddington <ben.biddington@gmail.com>
2021-12-16 07:54:50 +1300

    Change of approach

--- tests/day-14-examples.ts
+++ tests/day-14-examples.ts
@@ -57,43 +57,46 @@ const parse = (input: string) : [Template, Rules] => {
 }
 
 class Polymer {
-  private templateText: string;
-  private originalTemplateText: string;
+  private originalTemplate: string[];
   private rules: Rules;
+  private counts = new Map<string,number>();
+  private pairCounts = new Map<string,number>();
+  private value: string[];
 
   constructor(templateText: string, rules: Rules) {
-    this.templateText = templateText;
-    this.originalTemplateText = templateText;
+    this.value = templateText.split('');
+    this.originalTemplate = [...this.value];
     this.rules = rules;
+    this.increment(this.value);
   }
   
-  run(count: number): string {
-    let result = this.templateText;
+  run(count: number): void {
+    let result = this.value;
 
     for (let index = 0; index < count; index++) {
       result = this.step(result);
+      console.log({index, length: result.length});
     }
 
-    return this.templateText = result;
+    this.value = result
   }
 
   get length() {
-    return this.templateText.length;
+    return this.value.length;
+  }
+
+  toString() {
+    return this.value.join('');
   }
 
   reset() {
-    this.templateText = this.originalTemplateText;
+    this.counts.clear();
+    this.increment(this.originalTemplate);
+    this.value = [...this.originalTemplate]
   }
 
   mostCommon() : LetterCount {
-    const counts = new Map<string,number>();
-
-    for (let index = 0; index < this.templateText.length; index++) {
-      const letter = this.templateText.slice(index, index + 1);
-      counts.set(letter, (counts.get(letter) || 0) + 1);
-    }
-
-    const sorted = Array.from(counts.entries()).sort((a,b) => {
+    const sorted = Array.from(this.counts.entries()).sort((a,b) => {
       const [_, countA] = a;
       const [__, countB] = b;
 
@@ -104,39 +107,43 @@ class Polymer {
   }
 
   leastCommon() : LetterCount {
-    const counts = new Map<string,number>();
-
-    for (let index = 0; index < this.templateText.length; index++) {
-      const letter = this.templateText.slice(index, index + 1);
-      counts.set(letter, (counts.get(letter) || 0) + 1);
-    }
-
-    const sorted = Array.from(counts.entries()).sort((a,b) => {
+    const sorted = Array.from(this.counts.entries()).sort((a,b) => {
       const [_, countA]   = a;
       const [__, countB]  = b;
 
       return countB - countA;
     });
 
-    return { letter: sorted[sorted.length -1][0], count: sorted[sorted.length -1][1] };
+    return { 
+      letter: sorted[sorted.length -1][0], 
+      count: sorted[sorted.length -1][1] 
+    };
   }
 
-  private step(templateText: string) {
+  private step(template: string[]): string[] {
     const result = [];
-  
-    const template = templateText.split('');
   
     for (let index = 0; index < template.length - 1; index++) {
       const substring = template.slice(index, index + 2).join('');
-      
+
       if (this.rules.has(substring)) {
-        result.push(substring.slice(0, 1) + this.rules.get(substring));
+        const nextLetter = this.rules.get(substring);
+
+        this.increment([nextLetter]);
+
+        result.push(substring.slice(0, 1), nextLetter);
       }
     }
   
-    result.push(template.at(-1) || '');
+    result.push(template.at(-1) || ''); // [i] Final letter already counted
   
-    return result.join(''); 
+    return result;
+  }
+
+  private increment(letters: string[]) {
+    letters.forEach(letter => {
+      this.counts.set(letter, (this.counts.get(letter) || 0) + 1);
+    });
   }
 }
 
@@ -198,8 +205,10 @@ describe('--- Day 14: Extended Polymerization --- (part one)', () => {
     const [template, rules] = parse(input);
 
     const polymer = new Polymer(template.value, rules);
+    
+    polymer.run(4);
 
-    expect(polymer.run(4)).to.eql('NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB');
+    expect(polymer.toString()).to.eql('NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB');
 
     polymer.reset();
 
@@ -224,6 +233,21 @@ describe('--- Day 14: Extended Polymerization --- (part one)', () => {
     const polymer = new Polymer(template.value, rules);
 
     polymer.run(10);
+
+    const result = polymer.mostCommon().count - polymer.leastCommon().count;
+    
+    expect(result).to.eql(2068);
+  });
+
+  it.skip('part two', () => {
+
+    const input = fs.readFileSync('./input/fourteen').toString();
+
+    const [template, rules] = parse(input);
+
+    const polymer = new Polymer(template.value, rules);
+
+    polymer.run(40);
 
     const result = polymer.mostCommon().count - polymer.leastCommon().count;
     
